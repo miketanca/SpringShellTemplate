f9ea8c38e0847ea9664ade5e7d045865ef9128dc
Michael Tan <mtan@bdo.ca>
2024-03-24 22:46:54 -0400

    initial

---
+++ mi-network-connector/src/test/java/com/bns/clouddb/minetworkconnector/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bns.clouddb.minetworkconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
