d782ec35341023bd3e606eb2d7992ce8f2fe7051
Michael Tan <mtan@bdo.ca>
2024-04-04 16:10:16 -0400

    add help, more validation, get option from external file

--- mi-network-connector/src/main/java/com/bns/clouddb/minetworkconnector/CloudDBCommands.java
+++ mi-network-connector/src/main/java/com/bns/clouddb/minetworkconnector/CloudDBCommands.java
@@ -7,32 +7,55 @@ import lombok.SneakyThrows;
 import lombok.val;
 import lombok.var;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.util.Properties;
 
 @ShellComponent
 public class CloudDBCommands {
-    @Value("${fqdn}")
-    private String fqdn;
-
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @ShellMethod
+    @ShellMethod("Test connectivity to a SQL server managed instance and get latency.")
     @SneakyThrows
-    public void connect(
-            @ShellOption(defaultValue = "") String fqdn,
-            @ShellOption(defaultValue = "1433") int port,
-            @ShellOption(defaultValue = "10") int count,
-            @ShellOption(defaultValue = "5") int timeout) {
+    public void minc(
+            @ShellOption(defaultValue = "", help = "Fully qualified domain name of the SQL server managed instance")
+            String fqdn,
+            @ShellOption(defaultValue = "1433", help = "The TCP port of the SQL server managed instance, between 1 and 65535")
+            @Min(value = 1, message = "Port number should be greater or equal to 1")
+            @Max(value = 65535, message = "Port number should be less or equal to 65535")
+            int port,
+            @ShellOption(defaultValue = "10", help = "The count of connections to calculate average latency, between 1 and 20")
+            @Min(value = 1, message = "Count of connections should be greater or equal to 1")
+            @Max(value = 20, message = "Count of connections should be less or equal to 20")
+            int count,
+            @ShellOption(defaultValue = "5", help = "Timeout for connection to the SQL server managed instance, between 1 and 10 seconds")
+            @Min(value = 1, message = "Timeout should be greater or equal to 1 second")
+            @Max(value = 10, message = "Timeout should be less or equal to 10 seconds")
+            int timeout) {
 
-        val host = StringUtils.isBlank(fqdn) ? this.fqdn : fqdn;
+//        val path = Paths.get(".").toAbsolutePath().normalize().toString();
+        val props = new Properties();
+        props.load(new FileInputStream(new File("minc.properties")));
+        val hostProp = props.getProperty("sqlmi.default.fqdn");
+
+        val host = StringUtils.isBlank(fqdn) ? hostProp : fqdn;
+
+        if (StringUtils.isBlank(host)) {
+            System.out.println("Fqdn not found in \"minc.properties\" and not provided by command options");
+            return;
+        }
+
+        System.out.println("Connecting to " + host + ":" + port);
 
         InetAddress addr;
         try {
@@ -49,10 +72,9 @@ public class CloudDBCommands {
             return;
         }
 
-        System.out.println("Connecting to " + host + ":" + port);
         try {
-            var total = 0l;
-            for (var i = 0; i < Math.max(1, count); i++) {
+            long total = 0l;
+            for (int i = 0; i < Math.max(1, count); i++) {
                 total += getLatency(addr, port, timeout);
             }
             val result = new SuccessResult(
